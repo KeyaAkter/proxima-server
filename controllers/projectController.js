c4fd669a229ea40fbe0d306e32b65db146609925
KeyaAkter <sanhatanvin12@gmail.com>
2023-02-20 00:03:22 +0600

    update project error handled

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -76,6 +76,23 @@ const deleteProject = async (req, res) => {
 const updateProject = async (req, res) => {
   const { id } = req.params;
 
+  const { title, tech, budget, duration, manager, dev } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) emptyFields.push("title");
+  if (!tech) emptyFields.push("tech");
+  if (!budget) emptyFields.push("budget");
+  if (!duration) emptyFields.push("duration");
+  if (!manager) emptyFields.push("manager");
+  if (!dev) emptyFields.push("dev");
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all the fields!", emptyFields });
+  }
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "Invalid id" });
   }
@@ -83,7 +100,7 @@ const updateProject = async (req, res) => {
   const project = await Project.findOneAndUpdate(
     { _id: id },
     { ...req.body },
-    { new: "true" }
+    { new: true }
   );
 
   if (!project) {
