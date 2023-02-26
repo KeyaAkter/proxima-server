30c729973fd49cd4a74383e9b3f90a0331b2e078
KeyaAkter <sanhatanvin12@gmail.com>
2023-02-26 22:32:41 +0600

    login logic added

--- models/userModel.js
+++ models/userModel.js
@@ -16,6 +16,7 @@ const userSchema = new Schema({
   },
 });
 
+// signup
 userSchema.statics.signup = async function (email, password) {
   // validation
   if (!email || !password) {
@@ -49,6 +50,29 @@ userSchema.statics.signup = async function (email, password) {
     email,
     password: hash,
   });
+
+  return user;
+};
+
+// login
+
+userSchema.statics.login = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled!");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect Email!");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect Password!");
+  }
 
   return user;
 };
