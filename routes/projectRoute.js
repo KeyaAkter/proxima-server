7a95ba0380f6331e90eac111f18ddf2219e02720
KeyaAkter <sanhatanvin12@gmail.com>
2023-03-01 22:51:35 +0600

    authorization added

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -6,9 +6,12 @@ const {
   updateProject,
   deleteProject,
 } = require("../controllers/projectController");
+const requireAuth = require("../middlewares/requireAuth");
 
 // router
 const router = express.Router();
+
+router.use(requireAuth); // if the authentication is okay, then the next commands will execute
 
 // routes
 
